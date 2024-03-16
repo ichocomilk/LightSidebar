54c3e75dca1c34c39b36350c9b048459722acf13
iChocoMilk <ichocodev@gmail.com>
2024-03-16 01:22:20 -0300

    Add posibility of multiple sidebars without depend static packets

--- src/main/java/io/github/ichocomilk/lightsidebar/nms/v1_8R3/Sidebar1_8R3.java
+++ src/main/java/io/github/ichocomilk/lightsidebar/nms/v1_8R3/Sidebar1_8R3.java
@@ -10,6 +10,7 @@ import io.github.ichocomilk.lightsidebar.Sidebar;
 import net.minecraft.server.v1_8_R3.EntityPlayer;
 import net.minecraft.server.v1_8_R3.IScoreboardCriteria;
 import net.minecraft.server.v1_8_R3.MinecraftServer;
+import net.minecraft.server.v1_8_R3.NetworkManager;
 import net.minecraft.server.v1_8_R3.PacketPlayOutScoreboardDisplayObjective;
 import net.minecraft.server.v1_8_R3.PacketPlayOutScoreboardObjective;
 import net.minecraft.server.v1_8_R3.PacketPlayOutScoreboardScore;
@@ -18,9 +19,6 @@ import net.minecraft.server.v1_8_R3.Scoreboard;
 public final class Sidebar1_8R3 implements Sidebar {
 
     private static final Scoreboard SCOREBARD = MinecraftServer.getServer().getWorld().scoreboard;
-
-    private static final PacketPlayOutScoreboardObjective CREATE, DELETE;
-    private static final PacketPlayOutScoreboardDisplayObjective DISPLAY;
 
     private PacketPlayOutScoreboardScore[] lines;
     private final CustomObjective objective = new CustomObjective(SCOREBARD);
@@ -31,32 +29,34 @@ public final class Sidebar1_8R3 implements Sidebar {
         }
         final CustomObjective OBJECTIVE = new CustomObjective(SCOREBARD);
         SCOREBARD.setDisplaySlot(1, OBJECTIVE);
-        CREATE = new PacketPlayOutScoreboardObjective(OBJECTIVE, 0);
-        DELETE = new PacketPlayOutScoreboardObjective(OBJECTIVE, 1);
-        DISPLAY = new PacketPlayOutScoreboardDisplayObjective(1, OBJECTIVE);
     }
 
     @Override
     public void sendLines(Collection<Player> players) {
+        final PacketPlayOutScoreboardObjective delete = new PacketPlayOutScoreboardObjective(objective, 1);
+        final PacketPlayOutScoreboardObjective create = new PacketPlayOutScoreboardObjective(objective, 0);
+        final PacketPlayOutScoreboardDisplayObjective display = new PacketPlayOutScoreboardDisplayObjective(1, objective);
+
         for (final Player player : players) {
-            final EntityPlayer entityPlayer = ((CraftPlayer)player).getHandle();
-            entityPlayer.playerConnection.sendPacket(DELETE);
-            entityPlayer.playerConnection.sendPacket(CREATE);
-            entityPlayer.playerConnection.sendPacket(DISPLAY);
+            final NetworkManager connection = ((CraftPlayer)player).getHandle().playerConnection.networkManager;
+            connection.handle(delete);
+            connection.handle(create);
+            connection.handle(display);
             for (final PacketPlayOutScoreboardScore line : lines) {
-                entityPlayer.playerConnection.sendPacket(line);
+                connection.handle(line);
             }
         }
     }
 
     @Override
     public void sendLines(Player player) {
-        final EntityPlayer entityPlayer = ((CraftPlayer)player).getHandle();
-        entityPlayer.playerConnection.sendPacket(DELETE);
-        entityPlayer.playerConnection.sendPacket(CREATE);
-        entityPlayer.playerConnection.sendPacket(DISPLAY);
+        final NetworkManager connection = ((CraftPlayer)player).getHandle().playerConnection.networkManager;
+        connection.handle(new PacketPlayOutScoreboardObjective(objective, 1));
+        connection.handle(new PacketPlayOutScoreboardObjective(objective, 0));
+        connection.handle(new PacketPlayOutScoreboardDisplayObjective(1, objective));
+
         for (final PacketPlayOutScoreboardScore line : lines) {
-            entityPlayer.playerConnection.sendPacket(line);
+            connection.handle(line);
         }
     }
 
@@ -64,28 +64,28 @@ public final class Sidebar1_8R3 implements Sidebar {
     public void sendTitle(Collection<Player> players) {
         final PacketPlayOutScoreboardObjective packet = new PacketPlayOutScoreboardObjective(objective, 2);
         for (final Player player : players) {
-            ((CraftPlayer)player).getHandle().playerConnection.sendPacket(packet);
+            ((CraftPlayer)player).getHandle().playerConnection.networkManager.handle(packet);
         }
     }
 
     @Override
     public void sendTitle(Player player) {
         final PacketPlayOutScoreboardObjective packet = new PacketPlayOutScoreboardObjective(objective, 2);
-        ((CraftPlayer)player).getHandle().playerConnection.sendPacket(packet);
+        ((CraftPlayer)player).getHandle().playerConnection.networkManager.handle(packet);
     }
 
     @Override
     public void delete(Collection<Player> players) {
+        final PacketPlayOutScoreboardObjective delete = new PacketPlayOutScoreboardObjective(objective, 1);
         for (final Player player : players) {
-            final EntityPlayer entityPlayer = ((CraftPlayer)player).getHandle();
-            entityPlayer.playerConnection.sendPacket(DELETE);
+            ((CraftPlayer)player).getHandle().playerConnection.networkManager.handle(delete);
         }
     }
 
     @Override
     public void delete(Player player) {
         final EntityPlayer entityPlayer = ((CraftPlayer)player).getHandle();
-        entityPlayer.playerConnection.sendPacket(DELETE);
+        entityPlayer.playerConnection.networkManager.handle(new PacketPlayOutScoreboardObjective(objective, 1));
     }
 
     @Override
